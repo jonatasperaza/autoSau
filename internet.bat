fe126808b926ea98a415318090c7ca4473187176
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-11-18 16:01:24 -0300

    Adiciona loop de verificação de conexão com a internet e log de eventos

--- internet.bat
+++ internet.bat
@@ -1,16 +1,18 @@
 @echo off
 setlocal
 
-rem Defina o comando que você deseja executar se não houver conexão
-
+:inicio
 rem Tenta pingar o servidor DNS do Google
 ping -n 1 8.8.8.8 >nul
 if errorlevel 1 (
-    echo Sem conexão com a internet. Executando o comando...
+    echo [%date% %time%] Sem conexão com a internet. Executando login...
     curl -X POST -F "auth_user=" -F "auth_pass=" -F "accept=True" https://captive-portal.araquari.ifc.edu.br:8003/index.php?zone=vlan_40_route_v4
-
 ) else (
-    echo Conexão com a internet detectada.
+    echo [%date% %time%] Conexão com a internet detectada. Continuando...
 )
+
+rem Aguarda 10 segundos antes de repetir
+timeout /t 10 >nul
+goto inicio
 
 endlocal
